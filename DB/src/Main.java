f1006d15ef60a9b8e60f14f5b2d6b61056478a22
yoyis3000 <georgebarrios@umail.ucsb.edu>
2014-03-06 21:28:20 -0800

    First Commit on Eclipse

---
+++ DB/src/Main.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+import java.sql.*;
+
+public class Main {
+	static Connection Connect;
+	static Op Operate;
+	
+	public static void main (String[] args) throws SQLException {
+		try{
+			DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+		   }
+		catch(Exception e){
+			e.printStackTrace();
+							}
+		String stConnect = "jdbc:oracle:thin:@uml.cs.ucsb.edu:1521:xe";
+		String stUser = "georgebarrios";
+		String stPass = "5228986";
+		Connect = DriverManager.getConnection(stConnect,stUser,stPass);
+		Operate = new Op(Connect);
+		Statement stmt= Connect.createStatement();
+		//Scanner reader = new Scanner(System.in);
+		System.out.println("Testing if this actually works currently");
+	    String sql = "INSERT INTO ship_item " +
+	    			 "VALUES ('101','Puma','111',1,3,'Guatemala',5,8)";
+	    stmt.executeUpdate(sql);
+	   
+		
+		
+	}
+}
---
+++ DB/src/Op.java
@@ -0,0 +1,10 @@
+import java.sql.*;
+import java.text.SimpleDateFormat;
+
+public class Op {
+	public Connection con;
+	public Op(Connection con_1){
+		con = con_1;
+	}
+
+}
