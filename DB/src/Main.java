70ba9ad7eaeba4c633bc28c9646af3221cc54fad
yoyis3000 <georgebarrios@umail.ucsb.edu>
2014-03-06 22:18:33 -0800

    Ok everything seems to run now, done randomly commiting, serious mode now.

--- DB/src/Main.java
+++ DB/src/Main.java
@@ -18,13 +18,19 @@ public class Main {
 		String stPass = "5228986";
 		Connect = DriverManager.getConnection(stConnect,stUser,stPass);
 		Operate = new Op(Connect);
-		Statement stmt= Connect.createStatement();
+		//Statement stmt= Connect.createStatement();
 		//Scanner reader = new Scanner(System.in);
 		System.out.println("Testing if this actually works currently");
+
 	    //String sql = "INSERT INTO ship_item " +
 	    	//		 "VALUES ('101','Puma','111',1,3,'Guatemala',5,8)";
 	    System.out.println("test git");
 	   // stmt.executeUpdate(sql);
+
+	    String sql = "INSERT INTO ship_item " +
+	    			 "VALUES ('101','Puma','111',1,3,'Guatemala',5,8)";
+	   // stmt.executeUpdate(sql);
+
 	   
 		
 		
